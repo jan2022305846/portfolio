ed467bb5cd19f897fc607f20387d2e9f1e29ca9d
JannyAbuabu <abuabujanny99@gmail.com>
2026-02-07 10:31:59 +0800

    herosec update

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -20,7 +20,7 @@ export const HeroSection = () => {
             </span>
           </h1>
 
-          <p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-4">
+          <p className="text-lg md:text-xl text-muted-foreground font-bold max-2-2xl mx-auto opacity-0 animate-fade-in-delay-4">
             Networking • SysAdmin • Cybersecurity • Dev
           </p>
 
